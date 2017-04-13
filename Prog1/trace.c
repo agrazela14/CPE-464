cc238af53da8c7cc58eec3ee953d4516d30e0bb1
agrazela14 <agrazela@gmail.com>
2017-04-13 14:25:20 -0700

    probably reading correct dest and src mac addresses, now getting the type field and moving on from there

--- Prog1/trace.c
+++ Prog1/trace.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pcap/pcap.h>
+#include <netinet/ether.h>
+#include <arpa/inet.h>
 #include "trace.h"
 
 int main(int argc, char **argv) {
@@ -10,7 +12,7 @@ int main(int argc, char **argv) {
         return -1; 
     }
 
-    char *errBuf = malloc(PCAP_ERRBUF_SIZE); 
+    char errBuf[PCAP_ERRBUF_SIZE]; 
     pcap_t *traceFile = pcap_open_offline(argv[1], errBuf); 
     int result = 1;
 
@@ -20,7 +22,6 @@ int main(int argc, char **argv) {
         pcap_close(traceFile);
         return -2;
     }
-    free(errBuf);
 
     while (result > 0) {
         result = readPacket(traceFile);    
@@ -30,15 +31,30 @@ int main(int argc, char **argv) {
 
 
 int readPacket(pcap_t *traceFile) {
-    struct pcap_pkthdr *header;// = malloc(sizeof(struct pcap_pkthdr));
-    const unsigned char *data;// = malloc(4096);
+    struct pcap_pkthdr *header;
+    const unsigned char *data;
+    //const char *outStr;
+    //char *hostName;
     int res = pcap_next_ex(traceFile, &header, &data);
+    char *mac;
+    /*
+    //memcpy(destMac, data, 12);
+    for (int i = 0; i < 12; i += 2) {
+        memset((destMac + i), ntohs(*(data + i)), 2);
+    }
+    const struct ether_addr *destEthernet = (void *)data;
+    const struct ether_addr *srcEthernet = (void *)(data + 12);
+    */
+    //ether_line(outStr, ethernet, hostName);
 
     if (res > 0) {
         printf("Caplen:%d   Len:%d\n", header->caplen, header->len);
-        printf("%s\n\n", data); 
+
+        mac = ether_ntoa((void *)data);
+        printf("\tDest MAC: %s\n", mac); 
+        mac = ether_ntoa((void *)(data + sizeof(struct ether_addr)));
+        printf("\tSource MAC: %s\n", mac); 
+
     } 
-    //free(header);
-    //free(data);
     return res;
 }
