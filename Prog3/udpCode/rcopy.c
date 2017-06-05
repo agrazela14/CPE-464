856560dbfd636f476e546a1613f8c4268156f0d9
Alex Grazela <agrazela@gmail.com>
2017-06-04 22:21:56 -0700

    Start on proj 3, stat machine in place, need to get it to actually connect, then start out on the rest of things

---
+++ Prog3/udpCode/rcopy.c
@@ -0,0 +1,216 @@
+// Client side - UDP Code                   
+// By Hugh Smith    4/1/2017        
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include "networks.h"
+#include "cpe464.h"
+
+#define MAXBUF 80
+#define xstr(a) str(a)
+#define str(a) #a
+
+typedef enum State STATE;
+
+enum State {
+    START, FILENAME, WAIT_FOR_DATA, WAIT_FOR_SREJ_RESP, SHUTDOWN, DONE
+}; 
+
+typedef struct {
+    char destFile[255];
+    char srcFile[255];
+    uint32_t windowSize;
+    uint32_t bufSize;
+    double error;
+    char remoteMachine[255];
+    uint32_t remotePort;
+} arguments;
+
+void talkToServer(arguments *argu, int socketNum, struct sockaddr_in6 *server);
+void checkArgs(int argc, char * argv[], arguments *argu);
+STATE initConnection(arguments *argu, int sockNum, struct sockaddr_in6 *server);
+STATE sendFilename(arguments *argu, int sockNum, struct sockaddr_in6 *server);
+STATE readData(int *seqNum, FILE *outFile);
+STATE srejReadData(int *seqNum, FILE *outFile);
+int createPacket(char *buffer, uint32_t seqNum, uint32_t checksum, uint16_t flag, 
+ int dataSize, char *data);
+
+//int getData(char * buffer);
+
+
+int main (int argc, char *argv[])
+ {
+    int socketNum = 0;              
+    struct sockaddr_in6 server;     // Supports 4 and 6 but requires IPv6 struct
+    //int portNumber = 0;             // SockAddrLen used in recvfrom is sizeof(server)
+    arguments *argu = malloc(sizeof(arguments));
+    
+    checkArgs(argc, argv, argu);
+    
+    socketNum = setupUdpClientToServer(&server, argu->remoteMachine, argu->remotePort);
+    
+    talkToServer(argu, socketNum, &server);
+    free(argu);
+    return 0;
+}
+
+//Change this function to kick off communication
+void talkToServer(arguments *argu, int sockNum, struct sockaddr_in6 *server)
+{
+    STATE state = START;
+    int seqNum = 0;
+    FILE *copyFile = fopen(argu->destFile, "w+");
+
+    sendtoErr_init(argu->error, DROP_ON, FLIP_ON, DEBUG_ON, RSEED_ON);
+
+    while (state != DONE) {
+        
+        switch (state) {
+            case START:
+                state = initConnection(argu, sockNum, server);
+                break;
+
+            case FILENAME:
+                state = sendFilename(argu, sockNum, server);
+                break;
+
+            case WAIT_FOR_DATA:
+                state = readData(&seqNum, copyFile); 
+                break;
+
+            case WAIT_FOR_SREJ_RESP:
+                state = srejReadData(&seqNum, copyFile);
+                break;
+
+            case SHUTDOWN: //Do Cleanup
+                close(sockNum);
+                fclose(copyFile);
+                state = DONE;
+                break;
+
+            case DONE:
+                break;
+
+            default:
+                fprintf(stderr, "default in state switch, we'z zogged!\n");
+                state = SHUTDOWN;
+                break;
+        }
+    }
+}
+
+//Send the initial Packet
+//Read for initial packet success
+//If response comes go on to filename state
+//If timeout and resend 10 times, go to Shutdown state
+STATE initConnection(arguments *argu, int sockNum, struct sockaddr_in6 *server) {
+    unsigned int addrLen = sizeof(*server);
+    char buffer[16];
+    char recvBuffer[16];
+    int packetLen = createPacket(buffer, 0, 1, 0, 0, ""); 
+    printf("Checksum = %d\n", in_cksum((unsigned short *)buffer, 10));
+    //Now get a checksum value
+    packetLen = createPacket(buffer, 0, 1, in_cksum((unsigned short *)buffer, 10), 0, ""); 
+    ssize_t bytesSent = sendtoErr(sockNum, buffer, packetLen, 0, 
+     (struct sockaddr *)server, addrLen); 
+
+    ssize_t bytesRecv = recvfrom(sockNum, recvBuffer, 10, 0, 
+     (struct sockaddr *)server, &addrLen); 
+    printf("Recv Flag: %hd, bytes Recv: %d bytes Sent: %d\n", 
+     (short)recvBuffer[4], (int)bytesRecv, (int)bytesSent);
+    return DONE;
+} 
+
+//send Filename
+//Read for Filename response
+//If you get a response Go on to Wait_For_Data state
+//If you send fileName with no response 10 times go to Shutdown
+//If you get the badFileName response, print error and go to shutdown
+STATE sendFilename(arguments *argu, int sockNum, struct sockaddr_in6 *server) {
+    
+    return DONE;
+} 
+
+//Read for data packet
+//If init success packet, go back to Filename state
+//Else check that seqNum correct
+//If correct, send RRseqNum, inc seqNum, continue in this state
+//If too High, send SREJseqNum, go to Wait_for_srej_resp state
+//If too low, discard data and RRSeqNum - 1 (Think more about this one)
+STATE readData(int *seqNum, FILE *outFile) {
+
+    return DONE;
+}
+
+//Read data packets
+//If they aren't the data for seqNum, discard and do not respond
+//If it is data for seqNum, RRseqNum, go to Wait_for_data state
+STATE srejReadData(int *seqNum, FILE *outFile) {
+
+    return DONE;
+}
+
+/*
+int getData(char * buffer)
+{
+    // Read in the data
+    buffer[0] = '\0';
+    printf("Enter the data to send: ");
+    scanf("%" xstr(MAXBUF) "[^\n]%*[^\n]", buffer);
+    getc(stdin);  // eat the \n
+        
+    return (strlen(buffer)+ 1);
+}
+*/
+
+void checkArgs(int argc, char * argv[], arguments *argu)
+{
+    /* check command line arguments  */
+    char *endptr;
+    if (argc != 8)
+    {
+        printf("usage: %s local-to-file remot-from-file window-size buffer-size error-percent host-name port-number \n", argv[0]);
+        exit(1);
+    }
+    
+    // Checks args and returns port number
+    //int portNumber = 0;
+    strcpy(argu->destFile, argv[1]);
+    strcpy(argu->srcFile, argv[2]);
+    argu->windowSize = atoi(argv[3]);
+    argu->bufSize = atoi(argv[4]);
+    argu->error = strtod(argv[5], &endptr);
+    strcpy(argu->remoteMachine, argv[6]);
+    argu->remotePort = atoi(argv[7]);
+}
+
+//Puts the packet into buffer, returns the length of the packet
+int createPacket(char *buffer, uint32_t seqNum, uint32_t checksum, uint16_t flag, 
+ int dataSize, char *data) {
+    int ndx = 0;
+    seqNum = htonl(seqNum);
+    memcpy(buffer, &seqNum, sizeof(uint32_t)); 
+    ndx += sizeof(uint32_t);
+    memcpy(buffer, &checksum, sizeof(uint32_t)); 
+    ndx += sizeof(uint32_t);
+    memcpy(buffer, &flag, sizeof(uint16_t)); 
+    ndx += sizeof(uint16_t);
+    memcpy(buffer, data, dataSize);
+    return ndx + dataSize;
+}
+
+
+
+
---
+++ Prog3/udpCode/server.c
@@ -0,0 +1,182 @@
+/* Server side - UDP Code                   */
+/* By Hugh Smith    4/1/2017    */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "networks.h"
+#include "cpe464.h"
+
+#define MAXBUF 80
+
+typedef enum State STATE;
+
+enum State {
+    START, FILENAME, SEND_DATA, WAIT_FOR_ACK, WAIT_FOR_EOF_ACK, SHUTDOWN, DONE
+};
+
+
+void processClient(int socketNum);
+void printClientIP(struct sockaddr_in6 * client);
+int checkArgs(int argc, char *argv[], double *error);
+void stateLoop(struct sockaddr_in6 client, int sockNum);
+STATE initConnection(struct sockaddr_in6 *client, int sockNum);
+int createPacket(char *buffer, uint32_t seqNum, uint32_t checksum, uint16_t flag, 
+ int dataSize, char *data);
+
+
+int main ( int argc, char *argv[]  )
+{ 
+    int socketNum = 0;              
+    //struct sockaddr_in6 client; // Can be either IPv4 or 6
+    int portNumber = 0;
+    double error;
+
+    portNumber = checkArgs(argc, argv, &error);
+        
+    socketNum = udpServerSetup(portNumber);
+
+    processClient(socketNum);
+
+    close(socketNum);
+}
+
+void processClient(int socketNum)
+{
+    //int dataLen = 0; 
+    char buffer[MAXBUF + 1];      
+    struct sockaddr_in6 client;     
+    socklen_t clientAddrLen = sizeof(client); 
+    
+    buffer[0] = '\0';
+    while (buffer[0] != '.')
+    {
+        recvfrom(socketNum, buffer, MAXBUF, 0, (struct sockaddr *) &client, &clientAddrLen);
+    
+        printClientIP(&client);
+        stateLoop(client, socketNum);
+
+        /*
+        printf(" Len: %d %s\n", dataLen, buffer);
+
+        // just for fun send back to client number of bytes received
+        sprintf(buffer, "bytes: %d", dataLen);
+        safeSendto(socketNum, buffer, strlen(buffer)+1, 0, (struct sockaddr *) & client, clientAddrLen);
+        */
+
+    }
+}
+
+void stateLoop(struct sockaddr_in6 client, int sockNum) {
+    STATE state = START;
+    //int seqNum = 0;
+    //FILE *readFile; 
+
+    while (state != DONE) {
+        
+        switch (state) {
+            case (START):
+                state = initConnection(&client, sockNum);
+                break;
+                
+            case (FILENAME):
+                break;
+                
+            case (SEND_DATA):
+                break;
+
+            case (WAIT_FOR_ACK):
+                break;
+
+            case (WAIT_FOR_EOF_ACK):
+                break;
+
+            case (SHUTDOWN):
+                break;
+
+            case (DONE):
+                break;
+
+            default:
+                printf("Reached default in state loop, should not have happened\n");
+                break;
+
+        }
+    }
+} 
+
+STATE initConnection(struct sockaddr_in6 *client, int sockNum) {
+    char recvBuffer[MAXBUF + 1];
+    char sendBuffer[MAXBUF + 1];
+    socklen_t clientSize = sizeof(client);
+
+    ssize_t recvBytes = recvfrom(sockNum, recvBuffer, 10, 0, 
+     (struct sockaddr *)client, &clientSize); 
+    printf("Recv'd Bytes: %d\n", (int)recvBytes);
+    printf("checksum field on recv'd data %d\n", (uint32_t)recvBuffer[4]);
+    printf("checksum calc on recv'd data: %d\n", 
+     in_cksum((unsigned short *)recvBuffer, recvBytes));
+
+    int packetSize = createPacket(sendBuffer, 0, 0, 2, 0, ""); 
+    packetSize = createPacket(sendBuffer, 0, in_cksum((unsigned short *)sendBuffer, 10), 
+     2, 0, ""); 
+    
+    ssize_t bytesSent = sendtoErr(sockNum, sendBuffer, packetSize, 0, 
+     (struct sockaddr *)client, clientSize);
+
+    printf("Bytes Sent: %d\n", (int)bytesSent);
+    return DONE;
+} 
+
+void printClientIP(struct sockaddr_in6 * client)
+{
+    char ipString[INET6_ADDRSTRLEN];
+
+    inet_ntop(AF_INET6, &client->sin6_addr, ipString, sizeof(ipString));
+    printf("Client info - IP: %s Port: %d ", ipString, ntohs(client->sin6_port));
+    
+}
+
+int checkArgs(int argc, char *argv[], double *error)
+{
+    // Checks args and returns port number
+    int portNumber = 0;
+    char *endptr;
+
+    if (argc > 3)
+    {
+        fprintf(stderr, "Usage %s [error-percent] [optional port number]\n", argv[0]);
+        exit(-1);
+    }
+    
+    if (argc == 3)
+    {
+        portNumber = atoi(argv[2]);
+    }
+    
+    *error = strtod(argv[1], &endptr);
+    
+    return portNumber;
+}
+
+//Puts the packet into buffer, returns the length of the packet
+int createPacket(char *buffer, uint32_t seqNum, uint32_t checksum, uint16_t flag, 
+ int dataSize, char *data) {
+    int ndx = 0;
+    seqNum = htonl(seqNum);
+    memcpy(buffer, &seqNum, sizeof(uint32_t)); 
+    ndx += sizeof(uint32_t);
+    memcpy(buffer, &checksum, sizeof(uint32_t)); 
+    ndx += sizeof(uint32_t);
+    memcpy(buffer, &flag, sizeof(uint16_t)); 
+    ndx += sizeof(uint16_t);
+    memcpy(buffer, data, dataSize);
+    return ndx + dataSize;
+}
+
